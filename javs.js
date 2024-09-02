0b3a5187573c9b31089514163267c4c3dca61689
egwaj <egwajohnson.a@gmail.com>
2024-09-02 18:21:12 +0100

    question2 commit

--- javs.js
+++ javs.js
@@ -1,15 +1,39 @@
-function lifeInWeeks(yourAge) {
-    const now = new Date();
-    /************Don't change the code above************/    
-      const yourAge=prompt("Your Age?");
-      const ageInDays= (90-yourAge)*365;
-      const ageInWeeks= (90-yourAge)*52;
-      const ageInMonths=(90-yourAge)*12;  
 
-      alert(`You have ${ageInDays} days, ${ageInWeeks} Weeks and, ${ageInMonths} Months`);
-       
-    /*************Don't change the code below**********/
-    };
+// function lifeInWeeks(age) {
+//     /************Don't change the code above************/
+//     const yourAge = Number(prompt("Your Age?"));
+//     const ageInDays = (90 - yourAge) * 365;
+//     const ageInWeeks = (90 - yourAge) * 52;
+//     const ageInMonths = (90 - yourAge) * 12;
 
-    lifeInWeeks();
+//     alert(`You have ${ageInDays} days, ${ageInWeeks} Weeks and, ${ageInMonths} ,Months and you are ${yourAge}, Years Old`);
 
+//     /*************Don't change the code below**********/
+// };
+
+// lifeInWeeks();
+
+//  to run queston1 comment Question2 out.
+// Question2
+
+
+// console.log(love_calculator());
+// const prompt = require('prompt-sync')({sigint: true});
+function calculateLove() {
+    const name1 = prompt("Enter Your Name");
+    const name2 = prompt("Enter Your Partner Name");
+    var result;
+    
+    if (name1 === '' || name2 === '') {
+        alert('Please enter both names.');
+        return;
+    }
+
+    const matchPercentage = Math.floor(Math.random() * 101);
+
+   result =  `${name1} and ${name2} are ${matchPercentage}% match!!`;
+
+    alert (result);
+};
+
+   calculateLove();
